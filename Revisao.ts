0fe4408002d55d493eb183426a5111c3723206d1
Etec01 <you@example.com>
2019-08-14 21:45:41 -0300

    criação de função

--- Revisao.ts
+++ Revisao.ts
@@ -3,7 +3,11 @@ console.log(message)
 var n1 = 9;
 var n2 = 5;
 var n3 = 3;
-var res = (n1 + n2 + n3)/3;
-console.log(res);
+// var res = (n1 + n2 + n3)/3;
+ let fmedia = function(nota1:number,nota2:number,nota3:number):number{
+     return(nota1 + nota2 + nota3)/3
+ }
+
+console.log (`Oi eu sou GOku e minha media é : ${fmedia(n1,n2,n3)}`);
 
 
--- impl/Revisao.js
+++ impl/Revisao.js
@@ -3,5 +3,8 @@ console.log(message);
 var n1 = 9;
 var n2 = 5;
 var n3 = 3;
-var res = (n1 + n2 + n3) / 3;
-console.log(res);
+// var res = (n1 + n2 + n3)/3;
+var fmedia = function (nota1, nota2, nota3) {
+    return (nota1 + nota2 + nota3) / 3;
+};
+console.log("Oi eu sou GOku e minha media \u00E9 : " + fmedia(n1, n2, n3));
