ecb1c932ec10048ddc37bd7c291e0b616d0641a6
Etec01 <you@example.com>
2019-08-14 22:29:06 -0300

    o ultimo exercicio de volume  e o erro de nome limpo

---
+++ exercicio.ts
@@ -0,0 +1,7 @@
+var com = 23 
+var lar = 53
+var alt = 32
+
+var voll = (comprimento:number,largura:number,altura:number) => (comprimento*largura*altura)
+
+console.log(`O volume da minha caixa é : ${voll(com,lar,alt)}`)
---
+++ impl/exercicio.js
@@ -0,0 +1,5 @@
+var com = 23;
+var lar = 53;
+var alt = 32;
+var voll = function (comprimento, largura, altura) { return (comprimento * largura * altura); };
+console.log("O volume da minha caixa \u00E9 : " + voll(com, lar, alt));
