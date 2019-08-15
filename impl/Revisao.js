e63c49fe227c9289a299750e1945a9eb6ca6840f
Etec01 <you@example.com>
2019-08-14 21:57:01 -0300

    arrou funcion

--- impl/Revisao.js
+++ impl/Revisao.js
@@ -1,10 +1,12 @@
-var message = "OI Eu sou Goku e minha media é ";
-console.log(message);
+ //var message = "OI Eu sou Goku e minha media é ";
+//console.log(message);
 var n1 = 9;
 var n2 = 5;
 var n3 = 3;
 // var res = (n1 + n2 + n3)/3;
-var fmedia = function (nota1, nota2, nota3) {
-    return (nota1 + nota2 + nota3) / 3;
-};
+//var fmedia = function (nota1, nota2, nota3) {
+//   return (nota1 + nota2 + nota3) / 3;}
+
+var fmedia = (nota1, nota2, nota3) => (nota1 + nota2 + nota3) / 3;
+
 console.log("Oi eu sou GOku e minha media \u00E9 : " + fmedia(n1, n2, n3));
