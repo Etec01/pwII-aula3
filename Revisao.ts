11954fa7406e2326dae64950464b113c0832c321
Etec01 <you@example.com>
2019-08-14 22:14:42 -0300

    concerto do erro é colocar um valor padrão na variavel 3

--- Revisao.ts
+++ Revisao.ts
@@ -2,12 +2,13 @@ let message: string = "OI Eu sou Goku e minha media é "
 console.log(message)
 var n1 = 9;
 var n2 = 5;
-var n3 = 3;
+//var n3 = 3;
 // var res = (n1 + n2 + n3)/3;
- let fmedia = function(nota1:number,nota2:number,nota3:number):number{
-     return(nota1 + nota2 + nota3)/3
- }
+//let fmedia = function(nota1:number,nota2:number,nota3:number ):number{
+//    return(nota1 + nota2 + nota3)/3}
+//var fmedia = (nota1:number, nota2:number, nota3:number) => (nota1 + nota2 + nota3) / 3;
 
-console.log (`Oi eu sou GOku e minha media é : ${fmedia(n1,n2,n3)}`);
+var fmedia = (nota1:number, nota2:number, nota3:number = 5) => (nota1 + nota2 + nota3) / 3;
+console.log (`Oi eu sou GOku e minha media é : ${fmedia(n1,n2)}`);
 
 
--- impl/Revisao.js
+++ impl/Revisao.js
@@ -1,12 +1,14 @@
- //var message = "OI Eu sou Goku e minha media é ";
-//console.log(message);
+var message = "OI Eu sou Goku e minha media é ";
+console.log(message);
 var n1 = 9;
 var n2 = 5;
-var n3 = 3;
+//var n3 = 3;
 // var res = (n1 + n2 + n3)/3;
-//var fmedia = function (nota1, nota2, nota3) {
-//   return (nota1 + nota2 + nota3) / 3;}
-
-var fmedia = (nota1, nota2, nota3) => (nota1 + nota2 + nota3) / 3;
-
-console.log("Oi eu sou GOku e minha media \u00E9 : " + fmedia(n1, n2, n3));
+//let fmedia = function(nota1:number,nota2:number,nota3:number ):number{
+//    return(nota1 + nota2 + nota3)/3}
+//var fmedia = (nota1:number, nota2:number, nota3:number) => (nota1 + nota2 + nota3) / 3;
+var fmedia = function (nota1, nota2, nota3) {
+    if (nota3 === void 0) { nota3 = 5; }
+    return (nota1 + nota2 + nota3) / 3;
+};
+console.log("Oi eu sou GOku e minha media \u00E9 : " + fmedia(n1, n2));
